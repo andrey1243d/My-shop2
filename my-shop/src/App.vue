f58a1e630bcbf06d9042adb7bc3cb21d694a1a7c
Andre1352 <profilprofil91@gmail.com>
2025-08-08 18:58:28 +0200

    d

--- my-shop/src/App.vue
+++ my-shop/src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    <ProductList />
+    <div class="container">
+      <div class="row">
+          <ProductList />
+      </div>
+      <ReviewsComponent />
+    </div>
+    <FooterComponent />
   </div>
 </template>
 <script>
 import ProductList from './components/ProductList.vue';
+import ReviewsComponent from './components/ReviewsComponent.vue';
 export default {
   name: 'App',
   components: {
-    ProductList
+    ProductList,
+    ReviewsComponent
   }
 }
 </script>
